ab83f7df8e481969e2b80a3e6f972e65475e5479
unknown <shouryakothari2001@gmail.com>
2020-07-21 13:28:21 +0530

    Initial Commit

--- routes/savedProfile.js
+++ routes/savedProfile.js
@@ -1,11 +1,18 @@
-// require("dotenv/config");
-// var express = require("express");
-// var router = express.Router();
-// var User = require("../models/user");  
-// var cities = require("all-countries-and-cities-json");
-// var indianCities = cities["India"];
-// var mongoose = require("mongoose");
+require("dotenv/config");
+var express = require("express");
+var router = express.Router();
+var User = require("../models/user");  
+var cities = require("all-countries-and-cities-json");
+var indianCities = cities["India"];
+var mongoose = require("mongoose");
 
-// router.post("/savedProfile", function(req,res){
-//     User.findById()
-// })
+router.post("/savedUser/:id", function(req,res){
+    User.findById(req.params.id, function(err,savedUser){
+        if(err){
+            console.log(err);
+            res.redirect("back");
+        }else{
+            savedUser.push(savedUser);
+        }
+    });
+});
