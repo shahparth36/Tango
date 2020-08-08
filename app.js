bf5e45eb6e32d2ed0fbc29cef57ef760707caabb
shahparth36 <parthshah1936@gmail.com>
2020-08-08 17:00:22 +0530

    Minor Changes

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(footerRoutes);
 
 
 app.get("/test", function (req, res) {
-    res.render("loader")
+    res.render("reset");
 })
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
--- routes/explore.js
+++ routes/explore.js
@@ -1,17 +1,17 @@
 require("dotenv/config");
-var express      = require("express");
-var router       = express.Router();
-var User         = require("../models/user");  
-var cities       = require("all-countries-and-cities-json");
+var express = require("express");
+var router = express.Router();
+var User = require("../models/user");
+var cities = require("all-countries-and-cities-json");
 var indianCities = cities["India"];
-var middleware   = require("../middleware")
+var middleware = require("../middleware")
 
 
 router.get("/explore", middleware.isLoggedIn, function (req, res) {
     var perPage = 9;
     var pageQuery = parseInt(req.query.page);
     var pageNumber = pageQuery ? pageQuery : 1;
-    var noMatch = null; 
+    var noMatch = null;
     if (Object.keys(req.query).length > 0) {
         User.find(filter(req)).skip((perPage * pageNumber) - perPage).limit(perPage).exec(function (err, allUsers) {
             User.count().exec(function (err, count) {
@@ -21,8 +21,8 @@ router.get("/explore", middleware.isLoggedIn, function (req, res) {
                     return res.redirect("back");
                 }
                 else {
-                    if(allUsers.length < 1){
-                        req.flash("error","No user exists" );
+                    if (allUsers.length < 1) {
+                        req.flash("error", "No user exists according to your given criteria.");
                         return res.redirect("/explore");
                     }
                     res.render("explore", {
@@ -31,50 +31,50 @@ router.get("/explore", middleware.isLoggedIn, function (req, res) {
                         foundUser: shuffle(allUsers),
                         noMatch: noMatch,
                         indianCities: indianCities
-                    }) 
+                    })
                 }
             })
         })
-    } 
+    }
     else {
-    //Get all users      
-       User.find({_id:{$ne:req.user}}).skip((perPage * pageNumber) - perPage).limit(perPage).exec(function(err,foundUser){
-           User.count().exec(function (err, count) {
-               if (err) {
+        //Get all users      
+        User.find({ _id: { $ne: req.user } }).skip((perPage * pageNumber) - perPage).limit(perPage).exec(function (err, foundUser) {
+            User.count().exec(function (err, count) {
+                if (err) {
                     console.log(err);
                     res.redirect("back");
                 }
                 else {
                     // render explore.ejs file
                     res.render("explore", {
-                       current: pageNumber,
-                       pages: Math.ceil(count / perPage),
-                       foundUser: shuffle(foundUser),
-                       noMatch: noMatch,
-                       indianCities: indianCities
-                   });
+                        current: pageNumber,
+                        pages: Math.ceil(count / perPage),
+                        foundUser: shuffle(foundUser),
+                        noMatch: noMatch,
+                        indianCities: indianCities
+                    });
                 }
-           }) 
+            })
         });
     }
-  });
-  
-  router.post("/explore", function(req, res) {
+});
+
+router.post("/explore", function (req, res) {
     User.find({
         _id: {
             $ne: req.user
         }
-    }, function(err,foundUser){
+    }, function (err, foundUser) {
         if (err) {
             req.flash("error", "Something went wrong.");
             return res.redirect("back");
         }
-    
+
         return res.redirect("/explore");
     })
 });
 function filter(req) {
-    var search1 = {_id:{$ne:req.user}};
+    var search1 = { _id: { $ne: req.user } };
     if (req.query.fullName) {
         search1.fullName = new RegExp(escapeRegex(req.query.fullName), 'gi');
     }
@@ -85,7 +85,17 @@ function filter(req) {
         search1.sexuality = new RegExp(escapeRegex(req.query.sexuality), 'gi');
     }
     if (req.query.gender) {
-        search1.gender = new RegExp(escapeRegex(req.query.gender), "gi");
+        if (req.query.gender === "Male") {
+            search1.gender = "Male";
+        }
+
+        if (req.query.gender === "Female") {
+            search1.gender = "Female";
+        }
+
+        if (req.query.gender === "Others") {
+            search1.gender = "Others";
+        }
     }
     if (req.query.city) {
         search1.city = new RegExp(escapeRegex(req.query.city), "gi");
