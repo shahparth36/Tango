735169604a4f88d9075fa70928c60f08e844fd57
shahparth36 <parthshah1936@gmail.com>
2020-08-08 18:09:21 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(footerRoutes);
 
 
 app.get("/test", function (req, res) {
-    res.render("reset");
+    res.render("otp");
 })
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
